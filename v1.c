5c66d98ca61a1e51852feebeefafad849bb60b75
David Liu <davxliu@gmail.com>
2017-11-18 14:53:27 -0500

    findPower function

--- v1.c
+++ v1.c
@@ -1,4 +1,16 @@
-int findPower(float velocity){
+int findPower(float gyroVelocity, int motorVelocity){
+  int power = 0;
+  if (gyroVelocity > 700)
+  {
+    power = -65;
+    return power;
+  }
+  else if (gyroVelocity < -700)
+  {
+    power = 65;
+  }
+
+
 
 }
 
